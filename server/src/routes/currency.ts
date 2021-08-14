67c4d09610d7917c7afc380ae63a428ac3dc5abd
Dennis Durairaj <dennis.durairaj@gmail.com>
2021-08-14 15:37:23 +0200

    Create mock api to return hardcoded list of currencies

--- server/src/routes/currency.ts
+++ server/src/routes/currency.ts
@@ -2,6 +2,6 @@ import express from "express";
 import controller from "../controllers/currency";
 const router = express.Router();
 
-router.get("/posts", controller.getAllCurrencies);
+router.get("/currencies", controller.getAllCurrencies);
 
 export = router;
