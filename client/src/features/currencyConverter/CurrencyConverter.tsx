399eaeb6cd9a7474ca515615e402bdd1e12e29e9
Dennis Durairaj <dennis.durairaj@gmail.com>
2021-08-16 16:27:41 +0200

    Add spinner when initial data for currency list is being fetched

--- client/src/features/currencyConverter/CurrencyConverter.tsx
+++ client/src/features/currencyConverter/CurrencyConverter.tsx
@@ -10,6 +10,7 @@ import {
   Heading,
   FormControl,
   FormLabel,
+  Spinner,
 } from '@chakra-ui/react';
 import React from 'react';
 import { useQuery } from 'react-query';
@@ -75,129 +76,139 @@ const CurrencyConverter: React.FC = () => {
     setValue('amount', format(e.target.value).toLocaleString());
   };
 
-  if (currenciesQuery.isLoading === true) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <Box
       as="main"
       justifyContent={['start', 'center']}
       className={styles.container}
     >
-      <Box className={styles.heading} as="h1">
-        Currency Converter
-      </Box>
-      <Box className={styles.form} as="form" onSubmit={handleSubmit(onSubmit)}>
-        <Box className={styles.conversionContainer} as="div">
-          <FormControl>
-            <FormLabel
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            >
-              From
-            </FormLabel>
-            <CurrencyPicker
-              currencies={currenciesQuery.data.currencies}
-              defaultCurrency={{
-                iso: 'EUR',
-                currency_name: 'Euro',
-                is_obsolete: false,
-              }}
-              label="currencyFrom"
-              register={register}
-              required
-              setValue={setValue}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            >
-              To
-            </FormLabel>
-            <CurrencyPicker
-              currencies={currenciesQuery.data.currencies}
-              defaultCurrency={{
-                iso: 'USD',
-                currency_name: 'US dollar',
-                is_obsolete: false,
-              }}
-              label="currencyTo"
-              register={register}
-              required
-              setValue={setValue}
-            />
-          </FormControl>
+      {currenciesQuery.isLoading === true ? (
+        <Spinner size="xl" />
+      ) : (
+        <>
+          <Box className={styles.heading} as="h1">
+            Currency Converter
+          </Box>
+          <Box
+            className={styles.form}
+            as="form"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <Box className={styles.conversionContainer} as="div">
+              <FormControl>
+                <FormLabel
+                  onClick={(e) => {
+                    e.preventDefault();
+                  }}
+                >
+                  From
+                </FormLabel>
+                <CurrencyPicker
+                  currencies={currenciesQuery.data.currencies}
+                  defaultCurrency={{
+                    iso: 'EUR',
+                    currency_name: 'Euro',
+                    is_obsolete: false,
+                  }}
+                  label="currencyFrom"
+                  register={register}
+                  required
+                  setValue={setValue}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel
+                  onClick={(e) => {
+                    e.preventDefault();
+                  }}
+                >
+                  To
+                </FormLabel>
+                <CurrencyPicker
+                  currencies={currenciesQuery.data.currencies}
+                  defaultCurrency={{
+                    iso: 'USD',
+                    currency_name: 'US dollar',
+                    is_obsolete: false,
+                  }}
+                  label="currencyTo"
+                  register={register}
+                  required
+                  setValue={setValue}
+                />
+              </FormControl>
 
-          <Box className={styles.amountInput}>
-            <Controller
-              control={control}
-              name="amount"
-              rules={{
-                required: {
-                  value: true,
-                  message: 'Please enter an amount',
-                },
-                maxLength: {
-                  value: 15,
-                  message: 'Amount should be less than 100,000,000,000',
-                },
-                pattern: {
-                  value: /\d/,
-                  message: 'Please enter a valid number',
-                },
-              }}
-              render={({ field }) => (
-                <FormControl>
-                  <FormLabel>Amount</FormLabel>
-                  <Input
-                    maxLength={12}
-                    isInvalid={errors.amount && true}
-                    errorBorderColor="red.300"
-                    {...field}
-                    onBlur={onBlurHandler}
-                    autoComplete="off"
-                  />
-                </FormControl>
-              )}
-            />
-            {errors.amount && (
-              <Text color="red.300" overflowWrap="anywhere" fontSize="sm">
-                {errors.amount.message}
-              </Text>
-            )}
+              <Box className={styles.amountInput}>
+                <Controller
+                  control={control}
+                  name="amount"
+                  rules={{
+                    required: {
+                      value: true,
+                      message: 'Please enter an amount',
+                    },
+                    maxLength: {
+                      value: 15,
+                      message: 'Amount should be less than 100,000,000,000',
+                    },
+                    pattern: {
+                      value: /\d/,
+                      message: 'Please enter a valid number',
+                    },
+                  }}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormLabel>Amount</FormLabel>
+                      <Input
+                        maxLength={12}
+                        isInvalid={errors.amount && true}
+                        errorBorderColor="red.300"
+                        {...field}
+                        onBlur={onBlurHandler}
+                        autoComplete="off"
+                      />
+                    </FormControl>
+                  )}
+                />
+                {errors.amount && (
+                  <Text color="red.300" overflowWrap="anywhere" fontSize="sm">
+                    {errors.amount.message}
+                  </Text>
+                )}
+              </Box>
+              <Box as="div" className={styles.submitResult}>
+                <Button
+                  disabled={
+                    watchAmount.length === 0 || parseInt(watchAmount) === 0
+                  }
+                  type="submit"
+                  colorScheme="blue"
+                >
+                  Convert
+                </Button>
+                {conversionQuery.isFetched && (
+                  <Stat
+                    className={clsx(styles.conversionResultData, {
+                      [styles.fadeIn]: conversionQuery.isFetched,
+                    })}
+                  >
+                    <StatLabel color="gray.500">
+                      {format(conversionQuery.data.amount).toLocaleString()}{' '}
+                      {conversionQuery.data.from} =
+                    </StatLabel>
+                    <StatNumber overflowWrap="anywhere" color="green.400">
+                      {format(
+                        conversionQuery.data.mid.toFixed(4)
+                      ).toLocaleString()}{' '}
+                      {conversionQuery.data.to}
+                    </StatNumber>
+                  </Stat>
+                )}
+              </Box>
+            </Box>
           </Box>
-          <Box as="div" className={styles.submitResult}>
-            <Button
-              disabled={watchAmount.length === 0 || parseInt(watchAmount) === 0}
-              type="submit"
-              colorScheme="blue"
-            >
-              Convert
-            </Button>
-            {conversionQuery.isFetched && (
-              <Stat
-                className={clsx(styles.conversionResultData, {
-                  [styles.fadeIn]: conversionQuery.isFetched,
-                })}
-              >
-                <StatLabel color="gray.500">
-                  {format(conversionQuery.data.amount).toLocaleString()}{' '}
-                  {conversionQuery.data.from} =
-                </StatLabel>
-                <StatNumber overflowWrap="anywhere" color="green.400">
-                  {format(conversionQuery.data.mid.toFixed(4)).toLocaleString()}{' '}
-                  {conversionQuery.data.to}
-                </StatNumber>
-              </Stat>
-            )}
-          </Box>
-        </Box>
-      </Box>
+        </>
+      )}
     </Box>
   );
 };
